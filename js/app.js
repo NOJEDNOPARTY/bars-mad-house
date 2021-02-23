692f7021ed907621d58f5251504eac243cbf11d1
bohdan <bordyuh@gmail.com>
2021-02-23 10:14:12 +0200

    add tamplate

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var common = {
+	init: function() {
+		common.main();
+		common.owl();
+	},
+	main: function(){
+
+		
+
+	},
+	owl: function(){
+		
+	},
+};
+
+(function() {
+	common.init();
+}());
