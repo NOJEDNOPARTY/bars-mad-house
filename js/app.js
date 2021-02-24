5038deb67d46163ea618de7ec0ae2ec15db24794
bohdan <bordyuh@gmail.com>
2021-02-24 13:29:32 +0200

    add done work and send for test

--- js/app.js
+++ js/app.js
@@ -5,7 +5,41 @@ var common = {
 	},
 	main: function(){
 
-		
+		function fixPanel() {
+			var header  = $('.header');
+
+			if ($(window).scrollTop() > 0) {
+				header.addClass('fixed');
+			}else {
+				header.removeClass('fixed');
+			};
+		};
+
+		fixPanel();
+
+		$(window).scroll(function() {
+			fixPanel();
+		});
+
+		$( window ).resize(function() {
+			fixPanel();
+		});
+
+
+		setTimeout(function(){
+			$('body').addClass('show');
+		}, 700);
+
+		setTimeout(function(){
+			$('.main-img').addClass('show');
+		}, 1200);
+
+		$('.menu-trigger').click(function(event){
+			event.preventDefault();
+			$('.menu-trigger').toggleClass('open');
+			$('.header').toggleClass('open');
+			$('body').toggleClass('hidden');
+		});
 
 	},
 	owl: function(){
